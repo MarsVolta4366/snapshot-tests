b29251eeaa748de2508ee625b6eae2cf621da961
unknown <derek.slauson5@gmail.com>
2022-01-25 10:28:57 -0600

    Finished activity

---
+++ src/GitHubCard.js
@@ -0,0 +1,16 @@
+import Card from "react-bootstrap/Card"
+
+export default function GitHubCard() {
+    return (
+        <div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>MarsVolta4366</Card.Title>
+                    <Card.Text>
+                        I'm an aspiring full-stack developer!
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
